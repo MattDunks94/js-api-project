dee0dd263750eaf0544f95867f54849bcc16f84e
Matt <mjd014@live.co.uk>
2022-08-18 20:02:57 +0000

    created function that collects our form data and displays it in our modal body.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,19 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm() {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    })
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -11,7 +24,7 @@ async function getStatus(e) {
 
     const data = await response.json();
 
-    if(response.ok) {
+    if (response.ok) {
         displayStatus(data)
     } else {
         throw new Error(data.error);
@@ -27,4 +40,4 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
-}
+};
